bb22599769f042d81605624a20ecc70487d8c426
mishapodl <mishapod@gmail.com>
2019-08-20 00:38:59 +0300

    ---

--- main/main.js
+++ main/main.js
@@ -1,1 +1,23 @@
-console.log('Hello World');
+function Book(title, auth, year) {
+  this.title = title;
+  this.auth = auth;
+  this.year = year;
+}
+
+Book.prototype.getSummery = function() {
+  return `Title - ${this.title}, Auth - ${this.auth}, Year - ${
+    this.year
+  }, Month - ${this.month}, Day - ${this.day}`;
+};
+
+function Magazine(title, auth, year, month) {
+  Book.call(this, title, auth, year);
+
+  this.month = month;
+}
+
+Magazine.prototype = Object.create(Book.prototype);
+
+const book = new Magazine('451', 'Oruel', '1975', 'Jun');
+Magazine.prototype.constructor = Magazine;
+console.log(book);
